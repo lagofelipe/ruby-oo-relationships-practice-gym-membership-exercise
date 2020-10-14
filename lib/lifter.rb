0dc9e8cd75e202007cf429abfeb7dcfede622b3a
lagofelipe <lagofelipe@gmail.com>
2020-10-14 09:33:06 -0500

    done

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,39 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+def self.all
+@@all
 end
+
+def memberships
+Membership.all.select{|m|m.lifter == self}
+end
+
+def gyms
+memberships.map{|m|m.gym}
+end
+
+def self.all_lifters_average
+average = self.all.sum {|l|l.lift_total}
+average / @@all.length
+end
+
+def membership_cost
+  memberships.map{|m|m.cost}
+end
+
+#cost, name, lifter, gym
+def new_lifter(cost, name, gym)
+Membership.new(cost, name, self, gym)
+end
+
+########
+end####
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,34 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+#name, lift_total
+
+l1 = Lifter.new("Arnold Swatszaza", 1055)
+l2 = Lifter.new("Juicy Mike", 930)
+l3 = Lifter.new("Steroids John", 730)
+l4 = Lifter.new("Roids everyday Bob", 1001)
+l5 = Lifter.new("Juicying Carlos", 105)
+l6 = Lifter.new("Appolo the hustler", 825)
+
+g1 = Gym.new("Golds")
+g2 = Gym.new("Crunch cedar park")
+g3 = Gym.new("Planet Fitness")
+g4 = Gym.new("Golds Austin")
+g5 = Gym.new("Golds Cedar Park")
+g6 = Gym.new("Mega Golds Austin ")
+g7 = Gym.new("YMCA")
+g8 = Gym.new("Public Gym")
+
+#cost, name, lifter, gym
+
+m1 = Membership.new(55, "Premium", l1, g1)
+m2 = Membership.new(10, "Free", l2, g2)
+m3 = Membership.new(55, "Basic", l3, g3)
+m4 = Membership.new(1000, "Premium", l4, g4)
+m5 = Membership.new(55, "Basic", l5, g5)
+m6 = Membership.new(12, "Free", l6, g6)
+m7 = Membership.new(85, "Premium", l4, g8)
+
 binding.pry
 
-puts "Gains!"
+puts "==========================================================================Gains!"
